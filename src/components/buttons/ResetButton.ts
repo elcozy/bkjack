17628ac94abcba88f7cc424597daddd49397c53b
Chiemeka Madufor <61386343+elcozy@users.noreply.github.com>
2023-06-01 00:55:30 +0200

    added reset option

--- src/components/buttons/ResetButton.ts
+++ src/components/buttons/ResetButton.ts
@@ -4,29 +4,27 @@ import { SharedButton } from "./SharedButton";
 import { gsap } from "gsap";
 
 export class ResetButton {
-  public resetBtn: SharedButton;
+    public resetBtn: SharedButton;
 
-  constructor(private gamescene: GameScene) {
-    this.resetBtn = new SharedButton(
-      this.gamescene.tableWidth - 200,
-       90,
-      "RESET",
-      this.onResetClick.bind(this)
-    );
-    this.gamescene.addChild(this.resetBtn);
-  }
+    constructor(private gamescene: GameScene) {
+        this.resetBtn = new SharedButton(
+            this.gamescene.tableWidth - 200,
+            90,
+            "RESET",
+            this.onResetClick.bind(this)
+        );
+        this.gamescene.addChild(this.resetBtn);
+    }
 
-  private onResetClick() {
-    this.gamescene.showStartButton(false);
-    this.gamescene.userCount = 0;
-    this.gamescene.dealerCount = 0;
-    this.gamescene.clearTable();
-    Model.reset()
-Model.betScene.reset()
-    Model.gameStage.removeChild(Model.gameScene);
+    private onResetClick() {
+      this.gamescene.reset();
+        this.gamescene.showStartButton(false);
 
+        this.gamescene.clearTable();
+        Model.reset();
+        Model.betScene.reset();
+        Model.gameStage.removeChild(Model.gameScene);
 
-    Model.gameStage.addChild(Model.betScene);
-
-  }
+        Model.gameStage.addChild(Model.betScene);
+    }
 }
